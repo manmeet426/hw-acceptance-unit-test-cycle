3228184fb588f4b752861e353bc16a1fc17f4321
EC2 Default User <ec2-user@ip-172-31-38-175.us-east-2.compute.internal>
2018-10-05 22:33:30 +0000

    Initial commit

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
+  root 'movies#index'
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  # root 'movies#index'
+  # root :to => redirect('/index')
 end
